9d9c13764b83ec0d78c931154dcdc481231f7291
Şeyma Nur Aksoy <74136623+seymaNur367@users.noreply.github.com>
2021-04-30 18:59:54 +0300

    commit

---
+++ inheritanceHomeworkk/src/inheritanceHomeworkk/Instructor.java
@@ -0,0 +1,5 @@
+package inheritanceHomeworkk;
+
+public class Instructor {
+
+}
---
+++ inheritanceHomeworkk/src/inheritanceHomeworkk/Main.java
@@ -0,0 +1,10 @@
+package inheritanceHomeworkk;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
